4d48c4d0e6f2dabbeb3cb06d4c15f714c65bbc54
Gigizahak <114883103+Gigizahak@users.noreply.github.com>
2022-10-31 08:52:30 +0300

    Update js.js

--- Assets/js/js.js
+++ Assets/js/js.js
@@ -1,96 +1,30 @@
-//alert("Я")
-//console.log("z")
-//let name =prompt("ame", "Haidar")
-//alert(`Your name:${name}`);
-//let a=1, b=2;
-//let c= ++a;
-//let d=b++;
-//console.log(a);
-//console.log(b);
-//console.log(c);
-//console.log(d);
-    // a=''+1+0;
-    // b=''-1+0;
-    // c=true+false;
-    // d=6/'3';
-    // f='2'*'3';
-    // g=4+5+'px';
-    // h='$' + 4+5;
-    // i='4'-2;
-    // j='4px'-2;
-    // k="  -9  " +5;
-    // k1="  -9  " -5;
-    // l=null+1;
-    // m=undefined+1;
-    // n=" \t \n" -2;
-    // console.log(a,b,c,d,f,g,h,i,j,k,k1,l,m,n)
-// var a = prompt("number", 1);
-// var b = prompt("number", 2);
-// var c = Number(a) + Number(b);
-// alert(c)
-// num=3;
-// alert(num)
-    // a=10;
-    // b=2;
-    // c= a+b;
-    // d=a-b;
-    // f=a*b;
-    // i=a/b;
-    // console.log(c)
-    // console.log(d)
-    // console.log(f)
-    // console.log(i)
-// c=15;
-// d=2;
-// result=c+d;
-// alert(result)
-    // a=10
-    // b=2
-    // c=5
-    // result=a+b+c
-    // alert(result)
-// a=17
-// b=10
-// c=a-b
-// d=7
-// result=a+b+c+d
-    // str="Привет, Мир!"
-    // alert(str)
-// str1="Привет, "
-// str2="Мир"
-// alert(str1+str2)
-    // let name=prompt("name", "")
-    // alert(`Привет, ${name}!`)
-// let age = prompt("age", "")
-// alert(`Мне ${age} лет!`)
-    // str ='abcde'
-    // alert(str[0]+ str[2]+ str[4])
-// let num ='12345'
-// let sum = 0
-// i=0
-// while (num.length-i>0) {
-//     sum +=Number(num[0+i])
-//     i++
-// }
-// alert(sum)
-    // time_hour =60*60
-    // time_day = time_hour *24
-    // time_mount = time_day * 30
+// let array = ['Привет, ', 'мир','!'];
+// let arr = array.join(' ');
+// alert(arr)
+    // let array = ['Привет, ', 'мир','!'];
+    // let text = array
+    // let arr = text.join('');
+    // alert(arr)
 
-    // alert(`Час: ${time_hour}  День: ${time_day}  Месяц: ${time_mount}`)
-// let hour = prompt("Час", "");
-// let min = prompt("Минуты", "");
-// let secont = prompt("Секунды", "");
-// alert(`${hour}:${min}:${secont}`);
-    // number=24
-    // alert(24**2)
-// var num =47;
-// num+=7;
-// num-=18;
-// num*=10;
-// num/=15;
-// alert(num)
-    // var num =10;
-    // num= num++;
-    // num= num--;
-    // alert(num)
+    // let array = ['Привет, ', 'мир','!'];
+    // array[0] = 'Пока'
+    // alert(array)
+// var obj = {'Коля':'1000', 'Вася':'500', 'Петя':'200'};
+// alert(array)
+    // var arr = {
+    // 	'ru':['голубой', 'красный', 'зеленый'],
+    // 	'en':['blue', 'red', 'green'],
+    // };
+    // alert(arr['ru'][1])
+// arr = ['a', 'b', 'c'];
+// alert(arr)
+    // arr = ['a', 'b', 'c'];
+    // for (let i = 0; i<3; i++) {
+    //     console.log(arr[i])
+    // }
+// arr = ['a', 'b', 'c', 'd'] 
+// console.log(`${arr[0]}+${arr[1]}, ${arr[2]}+${arr[3]}`)
+    // arr = [2, 5, 3, 9] 
+    // console.log((arr[0]*arr[1])+(arr[2]*arr[3]))
+var obj = {a: 1, b: 2, c: 3};
+
